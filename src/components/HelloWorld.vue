ed0ce2fe1868ee33ae9ee77d6f5ad7fcee2591f3
Berkan <berkanoral25@gmail.com>
2024-07-25 15:34:22 +0300

    zvxcvx

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,24 @@
   <v-card class="fill-height  d-flex align-center">
     <v-card-text>
       <v-row justify="center">
-        <v-col cols="2 position-relative">
+        <v-col cols="10" sm="5" md="4" lg="3" xl="2" >
           <v-card height="500" width="100%" class="border-sm border-opacity-100 rounded-lg">
-            <v-card-text class="pa-0">
+            <v-card-text class="px-0">
               <v-row>
-                <v-col class="pt-10 px-10 border-b-sm">
-                  <v-text-field bg-color="grey-lighten-3"></v-text-field>
+                <v-col class="px-10 border-b-sm">
+                  <v-card height="90px" class="border-sm border border-opacity-10 d-flex align-end" readonly color="grey-lighten-3">
+                    <v-card-text>
+                      <v-row>
+                        <v-col>
+                          <v-row>
+                            <v-spacer />
+                            <v-col cols="auto" v-if="resultValue !== null" class="text-grey-darken-2">{{resultValue}}</v-col>
+                            <v-col cols="auto" v-else class="text-grey-darken-2"> {{ `${listForCount?.[0] ?? ''} ${operationList.find( e => e.operationName === operationValue)?.text ?? ''} ${listForCount?.[1] ?? ''}` }}</v-col>
+                          </v-row>
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
+                  </v-card>
                 </v-col>
               </v-row>
               <v-row>
@@ -16,12 +28,12 @@
                     <v-col cols="9">
                       <v-row>
                         <v-col v-for="(item, index) in operationList" :key="index" cols="4 py-1">
-                          <v-btn height="40px" color="grey-lighten-2">{{ item.text }}</v-btn>
+                          <v-btn height="40px" color="grey-lighten-2" @click="operationClick(item)" >{{ item.text }}</v-btn>
                         </v-col>
                       </v-row>
                     </v-col>
                     <v-col cols="3 py-1 fill-height">
-                      <v-btn height="100%" color="red-darken-1">AC</v-btn>
+                      <v-btn height="100%" color="red-darken-1" @click="resetCalculate()">AC</v-btn>
                     </v-col>
                   </v-row>
                 </v-col>
@@ -32,12 +44,12 @@
                     <v-col cols="9">
                       <v-row justify="center">
                         <v-col v-for="(item, index) in [1,2,3,4,5,6,7,8,9,0]" :key="index" cols="4 py-1">
-                          <v-btn height="40px" color="grey-lighten-2">{{ item }}</v-btn>
+                          <v-btn height="40px" color="grey-lighten-2" @click="numberClick(item)" >{{ item }}</v-btn>
                         </v-col>
                       </v-row>
                     </v-col>
                     <v-col cols="3 fill-height py-1">
-                      <v-btn height="100%" class="text-h5" color="grey-darken-1">=</v-btn>
+                      <v-btn height="100%" class="text-h5" color="grey-darken-1" @click="calculate()">=</v-btn>
                     </v-col>
                   </v-row>
                 </v-col>
@@ -55,5 +67,6 @@ import { useCalculateStore } from "@/store/calculateStore"
 import { storeToRefs } from "pinia";
 
 const calculateStore = useCalculateStore()
-const { operationList } = storeToRefs(calculateStore)
+const { operationList, operationValue, listForCount, resultValue } = storeToRefs(calculateStore)
+const { numberClick, operationClick, calculate, resetCalculate } = calculateStore
 </script>
--- src/store/calculateStore.ts
+++ src/store/calculateStore.ts
@@ -2,6 +2,9 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useCalculateStore = defineStore("calculateStore", () => {
+  const resultValue = ref(null as null | number);
+  const listForCount = ref([] as string[]);
+  const operationValue = ref(null as null | string);
   const operationList = ref([
     {
       text: "+",
@@ -24,9 +27,74 @@ export const useCalculateStore = defineStore("calculateStore", () => {
       operationName: "Modulo",
     },
     {
-      text: "xʸ",
+      text: "^",
       operationName: "Exponentiation",
     },
   ]);
-  return { operationList };
+
+  const numberClick = (_value: number) => {
+    if (operationValue.value === null) {
+      if (listForCount.value[0] === undefined)
+        listForCount.value[0] = String(_value);
+      else listForCount.value[0] += String(_value);
+    } else {
+      if (listForCount.value[1] === undefined)
+        listForCount.value[1] = String(_value);
+      else listForCount.value[1] += String(_value);
+    }
+    resultValue.value = null;
+  };
+
+  const operationClick = (_value: { text: string; operationName: string }) => {
+    if (
+      listForCount.value[0] !== undefined &&
+      listForCount.value[1] === undefined
+    )
+      operationValue.value = _value.operationName;
+    else operationValue.value = null;
+  };
+
+  const resetCalculate = () => {
+    listForCount.value = []
+    operationValue.value = null
+    resultValue.value = null
+  };
+
+  const calculate = () => {
+    if (listForCount.value[0] && listForCount.value[1] && operationValue) {
+      if (operationValue.value === "Addition")
+        resultValue.value =
+          Number(listForCount.value[0]) + Number(listForCount.value[1]);
+      else if (operationValue.value === "Subtraction")
+        resultValue.value =
+          Number(listForCount.value[0]) - Number(listForCount.value[1]);
+      else if (operationValue.value === "Multiplication")
+        resultValue.value =
+          Number(listForCount.value[0]) * Number(listForCount.value[1]);
+      else if (operationValue.value === "Division")
+        resultValue.value =
+          Number(listForCount.value[0]) / Number(listForCount.value[1]);
+      else if (operationValue.value === "Modulo")
+        resultValue.value =
+          Number(listForCount.value[0]) % Number(listForCount.value[1]);
+      else if (operationValue.value === "Exponentiation")
+        resultValue.value =
+          Number(listForCount.value[0]) ** Number(listForCount.value[1]);
+    console.log(resultValue.value)
+      operationValue.value = null
+      listForCount.value = [];
+      listForCount.value[0] = String(resultValue.value)
+    }
+  };
+
+  return {
+    resultValue,
+    listForCount,
+    operationValue,
+    operationList,
+    numberClick,
+    operationClick,
+    calculate,
+    resetCalculate,
+  };
 });
